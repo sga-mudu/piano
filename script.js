c35964edb366b0316078677a26f33b3393dffeff
sga-mudu <411221402@gms.ndhu.edu.tw>
2025-01-17 22:23:18 +0800

    mudu huurhun

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 import {keys} from "./keys.js";
 const allKeys = []; 
+const audioMap = {};
+
+//key: value 
+//{q: q.mp3, w: w.mp3...}
 
 document.addEventListener("DOMContentLoaded", () => {
     const pianoKeysContainer = document.querySelector(".piano-keys");
     createPianoKeys(pianoKeysContainer);
+    preloadAudio();
+    document.addEventListener("keydown", pressedKey);
+
+    pianoKeysContainer.querySelectorAll(".key").forEach((key) => {
+        key.addEventListener("click", () => playTuned(key.dataset.key));
+    });
 });
 
 const createPianoKeys = (container) => {
@@ -18,3 +28,40 @@ const createPianoKeys = (container) => {
         allKeys.push(displayKey);
     });
 }
+
+const preloadAudio = () =>{
+    allKeys.forEach((keyName) => { 
+        //user = {}
+        //user[name] = "Mudu"
+        //user = {name: "Mudu"}
+
+        audioMap[keyName] = new Audio(`./pianoKeys/${keyName}.mp3`);
+    });
+};
+
+const pressedKey = (e) =>{
+    const {mappedKey} = keys.find(({key}) => key === e.key);
+    const key = mappedKey || e.key;
+
+    if(allKeys.includes(key)){
+        playTuned(key);
+    }
+};
+
+const playTuned = (key) => {
+    const audio = audioMap[key];
+    if(!audio) return;
+    audio.currenTime = 0;
+
+    const volumeInput = document.querySelector(".volume-slider input");
+    audio.volume = volumeInput.value;
+
+    audio.play();
+
+    const clickedKey = document.querySelector(`[data-key = "${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => clickedKey.classList.remove("active"), 150);
+
+};
+
+
